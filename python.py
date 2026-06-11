c5f7c8b4299e2a30f347ef642f1f6ecc7fb72f0c
nashwavp4 <nashwavp4@gmail.com>
2026-06-11 15:40:06 +0530

    third commit

--- python.py
+++ python.py
@@ -79,8 +79,9 @@
 # else:
 #   print("senior citizen")
 
-for i in range(1,11):
-    print(i)  
-  
+# for i in range(1,11):
+#     print(i)  
 
-
+for i in range(1,101):
+    if i % 2==0:
+        print(i)
