721ca38d489c4549182833c00add9fbed1985689
nashwavp4 <nashwavp4@gmail.com>
2026-06-11 15:27:07 +0530

    second commit added pythonfile

---
+++ python.py
@@ -0,0 +1,86 @@
+# a=90
+# b=95
+# c=99
+# total=a+b+c
+# average=total/3
+# percentage=total/300*100
+# print("total=",total)
+# print("average=",average)
+# print("percentage=",percentage)
+
+# a=45
+# b=34
+# print(a,b)
+# x=input("enter a number")
+# print(x)
+# print(type(x))
+
+# x=5
+# print(type(x))
+# x=5.4
+# print(type(x))
+# x="hello"
+# print(type(x))
+# x=True
+# print(type(x))
+# x=None
+# print(type(x))
+
+# x=input()
+# print(type(x))
+
+# x=int(input("enter a number"))
+# print(type(x))
+# x=float(input(5))
+# print(type(x))
+
+# name=input("enter the name")
+# x=float(input("enter the mark in physics"))
+# y=float(input("enter the mark in maths"))
+# z=float(input("enter the mark in science"))
+# total=x+y+z
+# average=total/3
+# percentage=total/300*100
+# print("total=",total)
+# print("average=",average)
+# print("percentage=",percentage)
+
+# age=int(input("enter your age"))
+# if(age>=18):
+#   print("eligible")
+# else:
+#  print("not eligible")
+
+
+
+# a=int(input("enter a number:"))
+# b=int(input("enter a number:"))
+# c=int(input("enter a number:"))
+# print(a,b,c)
+# if a>b and a>c:
+#     print("a is the largest")
+# elif b>a and b>c:
+#     print("b is the largest")
+# elif c>a and c>b:
+#     print("c is the largest")
+# else:
+#     print("all are equal")
+
+# x=float(input("enter a number"))
+# print(x)
+# if x>0 and x<=8:
+#  print("kids")
+# elif x>8 and x<=18:
+#   print("junior")
+# elif x>13 and x<=19:
+#   print("teenager")
+# elif x>20 and x<=50:
+#   print("adult")
+# else:
+#   print("senior citizen")
+
+for i in range(1,11):
+    print(i)  
+  
+
+
